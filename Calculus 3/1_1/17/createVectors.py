d90c721c8cf1645edba1428b4a57766c60717925
Jabberwocky <mrfpgapro@gmail.com>
2022-12-07 21:33:40 -0800

    Problem Sets Finished Through #51

--- Calculus 3/1_1/17/createVectors.py
+++ Calculus 3/1_1/17/createVectors.py
@@ -4,8 +4,8 @@ import numpy as np
 # Create Vectors (Tail & Head) => (origin,, destination) To Help Evaluate The
 # Direction (Path Taken) of the Parametric Function Respective Of The Parameter
 def create_vector_points(origin, destination):
-    origin_x = 3 - 2*np.cos(origin)
-    origin_y = -5 + 3*np.sin(origin)
-    destination_x = (3 - 2*np.cos(destination)) - (3 - 2*np.cos(origin))
-    destination_y = (-5 + 3*np.sin(destination)) - (-5 + 3*np.sin(origin))
+    origin_x = np.exp(origin)
+    origin_y = np.exp(2*origin)
+    destination_x = (np.exp(destination)) - (np.exp(origin))
+    destination_y = (np.exp(2*destination)) - (np.exp(2*origin))
     return origin_x, origin_y, destination_x, destination_y
